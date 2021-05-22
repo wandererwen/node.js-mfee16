784b64ff145c5999f4850b8de0e87b10032f1285
wenpeng <wenpeng425@gmail.com>
2021-05-22 16:38:09 +0800

    add second method to sum.js

--- basic/sum.js
+++ basic/sum.js
@@ -4,12 +4,27 @@ let name = "wen";
 
 console.log(name);
 
+/* Algorithm */
+// O(1)
+
+/* method 1 */
 function sum(n){
     let sum = 0;
     for(i=1; i<=n; ++i)
         sum+=i;
     return sum;
 }
+console.time("SUM");
+console.timeEnd("SUM");
+
+/* method 2 from classmate#3 */
+function sum(m){
+    let i=1;
+    let total = (i+m)*m/2
+    return total;
+}
+console.time("SUM");
+console.timeEnd("SUM");
 
 console.log(sum(1)); // 1
 console.log(sum(2)); // 3

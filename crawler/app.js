2bd38372d4af864cfdb9aaa46e0545ecd77e63f5
wenpeng <wenpeng425@gmail.com>
2021-05-23 16:33:20 +0800

    add 3 new files

---
+++ crawler/app.js
@@ -0,0 +1,8 @@
+/*
+crawler
+    npm install 
+     -> 讀 package.json "depenDencies"
+     -> axios -> npm install axios
+                    -> axios / package.json "depenDencies"
+                        -> follow-redirects
+*/

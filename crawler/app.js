273a4cc33b5b130b41137ea165efb99acca6fc2a
wenpeng <wenpeng425@gmail.com>
2021-05-30 20:53:55 +0800

    include fs.readfile

--- crawler/app.js
+++ crawler/app.js
@@ -1,56 +1,50 @@
+/* url */
 // https://www.twse.com.tw/exchangeReport/STOCK_DAY
 // ?response=json
 // &date=20210528
 // &stockNo=2610
 
-const axios = require('axios').default;
+// npm i axios -> 需要安裝因是第三方套件
+// 引入 axios
+const axios = require("axios").default;
 
-// Make a request for a user with a given ID
-// axios.get('/user?ID=12345')
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // always executed
-//   });
+// TODO: 從 stock.txt 讀取股票代碼
+// npm i fsnode -> 不用安裝fs(filesystem)因node.js內建
+const fs = require("fs");
 
-// Optionally the request above could also be done as
-axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+/* callback */
+fs.readFile("stock.txt", "utf8", (error, data) => {
+  if (error) {
+    console.error("讀檔錯誤", error);
+    return;
+  }
+  axios
+  .get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
     params: {
-        response: JSON,
-        date: 20210528,
-        stockNo: 2603
-    }
+      response: JSON,
+      date: 20210528,
+      stockNo: data
+    },
   })
   .then(function (response) {
-    console.log(response.data);
+    if (response.data.stat === "OK");
+    console.log(response.data.date);
+    console.log(response.data.title);
   })
   .catch(function (error) {
     console.log(error);
   })
   .then(function () {
     // always executed
-  });  
+  });
+  console.log(`讀到的 stock code ${data}`);
+});
 
-// Want to use async/await? Add the `async` keyword to your outer function/method.
-// async function getUser() {
-//   try {
-//     const response = await axios.get('/user?ID=12345');
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
+// const stockCode = ???
 
 /*
 crawler
-    npm install 
+    npm install
      -> 讀 package.json "depenDencies"
      -> axios -> npm install axios
                     -> axios / package.json "depenDencies"

65602c6993f4e158002b69616f75ca54e350bed2
wenpeng <wenpeng425@gmail.com>
2021-06-05 12:54:54 +0800

    add fs-promise.js

---
+++ crawler/fs-promise.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+
+// 引入 promise 版的 fs
+const fs = require("fs/promises");
+const moment = require("moment");
+
+// 就不需要自己包裝了
+// function readFilePromise() {
+//     return new Promise((resolve, reject) => {
+//         fs.readFile("stock.txt", "utf8", (err, data) => {
+//             if (err) {
+//                 reject(err);
+//             }
+//             resolve(data);
+//         });
+//     });
+// }
+
+// 因為是 promise 版本，所以會回傳 promise 物件
+fs.readFile("stock.txt", "utf8")
+    .then((stockCode) => {
+        console.log("stockCode", stockCode)
+        return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+            params: {
+                response: JSON,
+                date: moment().format("YYYYMMDD"),
+                stockNo: stockCode,
+            },
+        });
+    })
+    .then((response) => {
+        if (response.data.stat === "OK") {
+            console.log(response.data.date);
+            console.log(response.data.title);
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });

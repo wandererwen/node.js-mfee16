38e26bd00753177fe4a327d9ee353a8e939d69d7
wenpeng <wenpeng425@gmail.com>
2021-06-05 13:16:35 +0800

    update method promisifyAll

--- crawler/app-bluebird.js
+++ crawler/app-bluebird.js
@@ -5,7 +5,7 @@ const Promise = require("bluebird"); // 覆蓋原生函式
 // console.log(Promise); 
 // const Promise = require("bluebird");
 
-/* 因為用bluebird，不用自己包 */
+/* 因用bluebird，不需要自己包 */
 // function readFilePromise() {
 //     return new Promise((resolve, reject) => {
 //         fs.readFile("stock.txt", "utf8", (err, data) => {
@@ -17,10 +17,18 @@ const Promise = require("bluebird"); // 覆蓋原生函式
 //     });
 // }
 
+/* 方法1：一個函式一個函式包 */
 // 用 bluebird 包 callback 版本的 readFile
-const readFileBlue = Promise.promisify(fs.readFile);
+// const readFileBlue = Promise.promisify(fs.readFile);
 
-readFileBlue("stock.txt", "utf8")
+/* 方法2：整個 fs 都包起來 */
+// 把 fs 所有的 function 都包成 promise
+// 但是原生的函式名稱後面會被加上 Async
+const fsBlue = Promise.promisifyAll(fs);
+// fsBlue.readFileAsync
+
+fsBlue
+.readFileAsync("stock.txt", "utf-8")
     .then((stockCode) => {
         console.log("stockCode:", stockCode);
         return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {

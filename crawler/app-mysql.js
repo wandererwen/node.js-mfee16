cdbbfef596955038419937c5b1d12b9ac57bd730
wenpeng <wenpeng425@gmail.com>
2021-06-06 10:41:20 +0800

    update db connection with dotenv

--- crawler/app-mysql.js
+++ crawler/app-mysql.js
@@ -10,13 +10,23 @@ const axios = require("axios");
 const fs = require("fs/promises");
 const mysql = require("mysql");
 const Promise = require("bluebird");
+require('dotenv').config();
 
 // db connection, password removed
+// let connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'stock',
+// });
+
+// db connection with dotenv
 let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'stock',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // mysql is callback-based, use bluebird to promisify

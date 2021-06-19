7bc73729ab2cd3de9f50f0c3e0f01cbaf7765b52
wenpeng <wenpeng425@gmail.com>
2021-06-19 17:26:02 +0800

    add fetch version

--- simple-express/public/javascripts/api.js
+++ simple-express/public/javascripts/api.js
@@ -1,24 +1,35 @@
-/* ajax */
-$.ajax({
-    type: "post",
-    url: "api/stocks",
-    dataType: "json",
-})
-.done(function(data){
-    console.log(data);
-})
-.always(function () {
-    
-});
-
-/* axios */
-axios.get('/api/stocks')
-    .then(function (response) {
-        console.log(response.data);
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-    .finally(function () {
-
+$(function () {
+    /* ajax */
+    $.ajax({
+        type: "GET",
+        // path refer to /routes/api
+        url: "/api/stock",
+    }).done(function (data) {
+        console.log(data);
+    }).always(function () {
+        console.log("done");
     });
+
+    /* axios -> promised-based */
+    // then or async / await
+    axios.get('/api/stock')
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .finally(function () {
+
+        });
+    
+    /* fetch -> promised-based */
+    // then or async / await
+    fetch('/api/stock')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+})
